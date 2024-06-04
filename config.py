908e720e10690a31d01f2d83df2d4a0c2e13e68f
bana8121 <101971569+bana8121@users.noreply.github.com>
2024-06-03 22:36:16 -0500

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from urllib import parse
 
-TOKEN = "MTIzMTgwNjYzNjgwMTI2NTcyNQ.GXxfUw.tYe9_yaQjpeBCM6zUUGkhOnZofGLZgeDeRoVJA"
-CLIENT_SECRET = "bgQoC8177evTtgmRceRdkHqXxOFPM6rc"
+TOKEN = "MTIzMTgwNjYzNjgwMTI2NTcyNQ.GNzGnn.C41HOPxQXqnW98axR5PvG-DSRvmz4NE3ZLDrvI"
+CLIENT_SECRET = "cCowLsZ8lw7kmV9iqL_H3qBl5ueV3WiK"
 REDIRECT_URI = "http://localhost:5000/oauth/callback"
 OAUTH_URL = f"https://discord.com/oauth2/authorize?client_id=1231806636801265725&response_type=code&redirect_uri={parse.quote(REDIRECT_URI)}&scope=identify+guilds+email"
 DISCORD = "https://discord.com/channels/@me"
